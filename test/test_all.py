05c04020354c0d070c69ce648c2acce50ae8eea5
Ben Gift <ben@weaveup.com>
2017-08-26 02:12:57 -0700

    added tests

---
+++ test/test_all.py
@@ -0,0 +1,13 @@
+import os.path, sys
+from lib import blocks
+sys.path.append(os.path.join(os.path.dirname(os.path.realpath(__file__)), os.pardir))
+
+def test_the_creation_of_a_grid():
+    # A grid holds the colors of the squares
+
+    # We need a map to return that color, which is just blue for now, but it does use a function to return it just in case we want to randomize the colors, which we do in reality
+    color_map = {'blue': lambda: (1, 128, 1)}
+
+    assert(len(blocks.create_grid(3, 3, color_map)) == 3)
+
+
