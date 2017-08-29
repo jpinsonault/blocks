d963ee29848fbf7e08e93db1ed2fef60ffa4f8d0
Ben Gift <ben@weaveup.com>
2017-08-28 19:58:51 -0700

    fix dockerfile bug with en0

--- docker_run.sh
+++ docker_run.sh
@@ -2,5 +2,5 @@ docker build -t hi .
 # start this socat thing that lets docker send data to the XQuartz
 socat TCP-LISTEN:6000,reuseaddr,fork UNIX-CLIENT:\"$DISPLAY\" &
 # get the IP address for the display
-docker run -e DISPLAY=$(ifconfig en0 | grep 'inet '|awk '{print $2}'):0 hi
+docker run -e DISPLAY=$((ifconfig en0 && ifconfig en1) | grep 'inet '|awk '{print $2}'):0 hi
 killall socat
